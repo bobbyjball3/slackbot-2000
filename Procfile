55e6ade06d9d1d34f21598eef83b75098cb599dd
Bobby Ball <bobbybal@amazon.com>
2018-01-18 11:23:38 -0800

    Bootstrapping repo for travis/heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node slackbot-2000.js
---
+++ test/mocha-dummy.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const expect = require('chai').expect
+
+describe('Dummy unit tests', () => {
+
+	describe('Dummy unit test', () => {
+
+		it('Should be true', (done) => {
+			expect(true).to.be.true
+			done()
+		})
+
+	})
+
+})
